b2fd32dad306cdbbfd6a5c6db62d327b5636f0a9
Pooja Patil <poojahanama@vmware.com>
2019-12-25 14:10:15 +0530

    Quiz end

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
     render() {
+        const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
         return (
             <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[0]} />
+                {!isQuizEnd && <QuizQuestion quiz_question={quizData.quiz_questions[0]} />}
+                {isQuizEnd &&  <QuizEnd />}
             </div>
         );
     }
